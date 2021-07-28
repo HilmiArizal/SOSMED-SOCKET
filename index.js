9aa653690c4cdbaf4f9be02c1b9d9de702f2d00d
Hilmi Arizal <hilmi.arizal36@gmail.com>
2021-07-28 14:46:37 +0700

    change-portlisten

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     res.send(`SERVER IS RUNNING IN PORT ${PORT}`);
-})
+});
+
+app.listen(PORT, () => console.log(`SERVER SOCKET RUNNING IN PORT ${PORT}`));
 
 // app.use(router);
 
